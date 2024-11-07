3e26acf5c923d7e39ce1e3765ccf744d7078bd19
Yajme <spartan.emjayrongavilla@gmail.com>
2024-11-07 15:56:36 +0800

    preparation of deployment to cloud

--- controllers/userController.js
+++ controllers/userController.js
@@ -7,7 +7,9 @@ const authenticateUser = async (req,res,next)=>{
     try{
         const username = req.body.username;
         const password= req.body.password;
-        const rows = await connection.query('SELECT * FROM users WHERE username = $1 AND deleted_at is not NULL',[username]);
+
+        const rows = await connection.query('SELECT * FROM users WHERE username = $1 AND deleted_at is NULL',[username]);
+        
         if(rows.rowCount < 1) return res.status(404).json({message : 'User not found'});
         const user = rows.rows[0];
         if(password !== user.password) return res.status(401).json({message : "Invalid password"});
